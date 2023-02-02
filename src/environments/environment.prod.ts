3c80616842cb672753c5040248b5473cde9a68d4
AlexanderCont <55602935+YotzinAlexander@users.noreply.github.com>
2023-02-02 08:56:43 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,7 @@ export const environment = {
   /** URL DE SERVIDOR, AQUI PUEDE IR LA LOCAL O SERVIDOR EN USO CON SU PUERTO EN FUNCIONAMIENTO */
   /** LOS DOS ARCHIVOS ENVIRONMENT TIENE QUE TENER LAS MIMAS RUTAS DE SERVIDOR */
   production: true,
-  // url: 'http://189.254.158.196:8080',
-   url: 'http://10.11.124.235:2000',
+   url: 'http://0.0.0.0:2000',
    noti: 'https://onesignal.com/api/v1/notifications',
 
-
-
-
-
-   // url: 'http://189.211.127.34:8080'
 };
